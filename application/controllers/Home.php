13bafdcc0ce9a6901628dca1e00dbe23f0a3e6bb
Ryan Hartadi <Ryanhartadi06@gmail.com>
2020-10-26 14:33:13 +0700

    Update Seach

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -11,12 +11,55 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             // belumlogin();
         }
         public function index(){
-            $data['kategori'] = $this->v->getData('kategori');
-            $data['terbaru'] = $this->m->getTerbaru();
-            $data['populer'] = $this->m->getPopuler();
-            $this->load->view("template_user/header");
-            $this->load->view("User/Home",$data);
-            $this->load->view("template_user/footer");
+            $this->form_validation->set_rules('search', 'Search', 'required');
+            if ($this->form_validation->run() == false) {
+                $data['kategori'] = $this->v->getData('kategori');
+                $data['terbaru'] = $this->m->getTerbaru();
+                $data['populer'] = $this->m->getPopuler();
+                $this->load->view("template_user/header");
+                $this->load->view("User/Home",$data);
+                $this->load->view("template_user/footer");
+            }else {  
+                $search = $this->input->post('search');
+                $category = $this->input->post('category');
+                $data['title'] = "MyHouse - Property Group";
+                $this->load->library('pagination');
+                $total_rows		= $this->db->query("SELECT * FROM rumah")->num_rows();
+                
+                $config['base_url'] 	= base_URL().'listing/index/';
+                $config['total_rows'] 	= $total_rows;
+                $config['uri_segment'] 	= 3;
+                $config['per_page'] 	= 2; 
+                $config['first_link']       = '<i class="fa fa-angle-right"></i>';
+                $config['last_link']        = 'Last';
+                $config['next_link']        = '<i class="fa fa-angle-right"></i>';
+                $config['prev_link']        = 'Prev';
+                $config['full_tag_open']    = '<ul class="pagination p-center">';
+                $config['full_tag_close']   = '</ul>';
+                $config['num_tag_open']     = '<li class="page-item"><span class="page-link">';
+                $config['num_tag_close']    = '</span></li>';
+                $config['cur_tag_open']     = '<li class="page-item active"><span class="page-link">';
+                $config['cur_tag_close']    = '</span></li>';
+                $config['next_tag_open']    = '<li class="page-item"><span class="page-link">';
+                $config['next_tagl_close']  = '</span></li>';
+                $config['prev_tag_open']    = '<li class="page-item"><span class="page-link">';
+                $config['prev_tagl_close']  = '</span></li>';
+                $config['first_tag_open']   = '<li class="page-item"><span class="page-link">';
+                $config['first_tagl_close'] = '</span></li>';
+                $config['last_tag_open']    = '<li class="page-item"><span class="page-link">';
+                $config['last_tagl_close']  = '</span></li>';
+                $this->pagination->initialize($config); 
+                $awal	= $this->uri->segment(3); 
+                if (empty($awal) || $awal == 1) { $awal = 0; } { $awal = $awal; }
+                $akhir	= $config['per_page'];
+                $data['pagi']	= $this->pagination->create_links();
+
+                $data['listing'] = $this->db->query("SELECT * FROM rumah , kategori WHERE (nama_pemilik_rumah LIKE '$search%' OR alamat_lengkap LIKE '$search%' OR kondisi LIKE '$search%' OR harga LIKE '$search%' OR name LIKE '$search%') AND rumah.id_kategori = kategori.id AND status_property = '$category'")->result();
+                // echo $category;
+                $this->load->view("template_user/header_two.php",$data);
+                $this->load->view("User/Listing.php",$data);
+                $this->load->view("template_user/footer.php");
+            }
         }
     }
     ?>
--- application/views/User/Home.php
+++ application/views/User/Home.php
@@ -1,218 +1,213 @@
+<!-- End Navigation -->
+<div class="clearfix"></div>
 
-		<!-- End Navigation -->
-		<div class="clearfix"></div>
+<div class="image-cover hero_banner half_banner" style="background:url(<?= base_url() ?>global/img/jpeg/banner.png) no-repeat;" data-overlay="1">
+  <div class="container my-4">
 
-		<div class="image-cover hero_banner half_banner" style="background:url(<?= base_url()?>global/img/jpeg/banner.png) no-repeat;"
-			data-overlay="1">
-			<div class="container my-4">
+    <h2 class="text-center mb-0">Find Your Perfect Home</h2>
+    <div class="mb-4 text-mlixer text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+    </div>
+    <form action="" method="post" enctype="multipart/form-data">
+    <div class="row justify-content-center">
+      <div class="col-lg-9 col-md-10">
+        <div class="full_search_box nexio_search lightanic_search hero_search-radius style-2">
+          <div class="search_hero_wrapping">
+            <div class="row">
+              <div class="col-lg-2 col-md-4 col-sm-4 small-padd">
+                <div class="form-group">
+                  <select class="form-control b-0 b-r" id="category" name="category">
+                    <option value="1">Dijual</option>
+                    <option value="2">Disewakan</option>
+                  </select>
+                </div>
+              </div>
+              <div class="col-lg-8 col-md-5 col-sm-12 small-padd">
+                <div class="form-group">
+                  <input name="search" id="search"type="text" class="form-control b-0" placeholder="Masukkan Kata Kunci">
+                </div>
+              </div>
+              
+              <div class="col-lg-2 col-md-3 col-sm-12 small-padd">
+                <div class="form-group">
+                <button type="submit" class="btn search-btn btn-dark">
+                 <span class="text">Cari</span>
+                </button>
+                </div>
+              </div>
+            
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </form>
+  </div>
+</div>
 
-				<h2 class="text-center mb-0">Find Your Perfect Home</h2>
-				<div class="mb-4 text-mlixer text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-				</div>
+<div class="clearfix"></div>
+<section>
+  <div class="container">
 
-				<div class="row justify-content-center">
-					<div class="col-lg-9 col-md-10">
+    <div class="row justify-content-center">
+      <div class="col-lg-7 col-md-8">
+        <div class="sec-heading center">
+          <h2>Cari Keperluan anda</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+        </div>
+      </div>
+    </div>
 
-						<div class="full_search_box nexio_search lightanic_search hero_search-radius style-2">
-							<div class="search_hero_wrapping">
-								<div class="row">
-									<div class="col-lg-2 col-md-4 col-sm-4 small-padd">
-										<div class="form-group">
-											<select class="form-control b-0 b-r" name="category">
-												<option value="0">Dijual</option>
-												<option value="1">Disewakan</option>
-											</select>
-										</div>
-									</div>
-									<div class="col-lg-8 col-md-5 col-sm-12 small-padd">
-										<div class="form-group">
-											<input type="text" class="form-control b-0"
-												placeholder="Masukkan Kata Kunci">
-										</div>
-									</div>
-									<div class="col-lg-2 col-md-3 col-sm-12 small-padd">
-										<div class="form-group">
-											<a href="#" class="btn search-btn btn-dark">Cari</a>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+    <div class="row">
+      <?php foreach ($kategori as $k) { ?>
+        <div class="col-lg-3 col-md-4">
+          <div class="property_location">
+            <div class="property_location_thumb">
+              <a href="#"><img src="<?= base_url('uploads/kategori/') . $k['image']; ?>" class="img-fluid rounded-lg" alt="" /></a>
+            </div>
+            <div class="property_location_caption">
+              <div class="property_location__first">
+                <h4><?= $k['name'] ?></h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      <?php } ?>
 
-		<div class="clearfix"></div>
-		<section>
-			<div class="container">
+    </div>
+  </div>
+</section>
+<div class="clearfix"></div>
 
-				<div class="row justify-content-center">
-					<div class="col-lg-7 col-md-8">
-						<div class="sec-heading center">
-							<h2>Cari Keperluan anda</h2>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-								ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-						</div>
-					</div>
-				</div>
+<section class="gray-simple min">
+  <div class="container">
 
-				<div class="row">
-					<?php foreach($kategori as $k){?>
-						<div class="col-lg-3 col-md-4">
-						<div class="property_location">
-							<div class="property_location_thumb">
-								<a href="#"><img src="<?= base_url('uploads/kategori/') . $k['image']; ?>" class="img-fluid rounded-lg" alt="" /></a>
-							</div>
-							<div class="property_location_caption">
-								<div class="property_location__first">
-									<h4><?= $k['name']?></h4>
-								</div>
-							</div>
-						</div>
-					</div>
-					<?php } ?>
+    <div class="row justify-content-center">
+      <div class="col-lg-7 col-md-8">
+        <div class="sec-heading center">
+          <h2>Terbaru</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+        </div>
+      </div>
+    </div>
 
-				</div>
-			</div>
-		</section>
-		<div class="clearfix"></div>
+    <div class="row">
+      <?php foreach ($terbaru as $t) { ?>
+        <div class="col-lg-4 col-md-6 col-sm-12">
+          <div class="list-slide-box">
+            <div class="modern-list">
+              <div class="grid-category-thumb">
+                <a href="#" class="overlay-cate"><img src="<?= base_url('uploads/rumah/') . $t['banner']; ?>" class="img-responsive" alt=""></a>
+                <div class="property_meta simple">
+                  <?php if ($t['status_property'] == 1) { ?>
+                    <a href="#" class="cate-trix theme-cl">Dijual</a>
+                  <?php } else { ?>
+                    <a href="#" class="cate-trix theme-cl">Sewa</a>
+                  <?php } ?>
 
-		<section class="gray-simple min">
-			<div class="container">
+                </div>
+              </div>
+              <div class="modern-list-content">
+                <div class="listing-content-wrap smalls text-center">
+                  <h4 class="lst-title"><a href="#"><?= $t['nama_pemilik_rumah'] ?></a>
+                  </h4>
+                  <p><?= $t['alamat_lengkap'] ?></p>
+                  <p class="btn btn-sm rounded-custom py-2 px-2 mt-2"><?= $t['harga'] ?></p>
+                </div>
+                <div class="listing-footer-wrap property-lists mt-2 d-flex justify-content-center">
+                  <div class="property-lists d-flex justify-content-center">
+                    <ul>
+                      <li><span class="flatcons"><img src="<?= base_url() ?>global/img/color-bed.svg" alt=""></span><?= $t['jumlah_kamar'] ?> Bed</li>
+                      <li><span class="flatcons"><img src="<?= base_url() ?>global/img/color-bathroom.svg" alt=""></span><?= $t['kamar_mandi'] ?> Bath</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      <?php } ?>
+      <!-- Single Places -->
 
-				<div class="row justify-content-center">
-					<div class="col-lg-7 col-md-8">
-						<div class="sec-heading center">
-							<h2>Terbaru</h2>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-								ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-						</div>
-					</div>
-				</div>
+    </div>
 
-				<div class="row">
-					<?php foreach($terbaru as $t){?>
-						<div class="col-lg-4 col-md-6 col-sm-12">
-						<div class="list-slide-box">
-							<div class="modern-list">
-								<div class="grid-category-thumb">
-									<a href="#" class="overlay-cate"><img src="<?= base_url('uploads/rumah/') . $t['banner']; ?>"
-											class="img-responsive" alt=""></a>
-									<div class="property_meta simple">
-									<?php if($t['status_property'] == 1){?>
-										<a href="#" class="cate-trix theme-cl">Dijual</a>
-									<?php }else { ?>
-										<a href="#" class="cate-trix theme-cl">Sewa</a>
-									<?php } ?>
-									
-									</div>
-								</div>
-								<div class="modern-list-content">
-									<div class="listing-content-wrap smalls text-center">
-										<h4 class="lst-title"><a href="#"><?=$t['nama_pemilik_rumah']?></a>
-										</h4>
-										<p><?= $t['alamat_lengkap']?></p>
-										<p class="btn btn-sm rounded-custom py-2 px-2 mt-2"><?= $t['harga']?></p>
-									</div>
-									<div class="listing-footer-wrap property-lists mt-2 d-flex justify-content-center">
-										<div class="property-lists d-flex justify-content-center">
-											<ul>
-												<li><span class="flatcons"><img src="<?= base_url()?>global/img/color-bed.svg"
-															alt=""></span><?= $t['jumlah_kamar']?> Bed</li>
-												<li><span class="flatcons"><img src="<?= base_url()?>global/img/color-bathroom.svg"
-															alt=""></span><?= $t['kamar_mandi']?> Bath</li>
-											</ul>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<?php } ?>
-					<!-- Single Places -->
-					
-				</div>
+    <div class="row">
+      <div class="col-lg-12 col-md-12 col-sm-12 text-center mt-3">
+        <a href="#" class="btn btn-theme bg-1">Selengkapnya</a>
+      </div>
+    </div>
 
-				<div class="row">
-					<div class="col-lg-12 col-md-12 col-sm-12 text-center mt-3">
-						<a href="#" class="btn btn-theme bg-1">Selengkapnya</a>
-					</div>
-				</div>
+  </div>
+</section>
+<div class="clearfix"></div>
+<section>
+  <div class="container">
 
-			</div>
-		</section>
-		<div class="clearfix"></div>
-		<section>
-			<div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-7 col-md-8">
+        <div class="sec-heading center">
+          <h2>Populer</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+        </div>
+      </div>
+    </div>
 
-				<div class="row justify-content-center">
-					<div class="col-lg-7 col-md-8">
-						<div class="sec-heading center">
-							<h2>Populer</h2>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-								ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-						</div>
-					</div>
-				</div>
+    <div class="row">
+      <?php foreach ($populer as $p) { ?>
+        <div class="col-lg-4 col-md-6 col-sm-12">
+          <div class="list-slide-box">
+            <div class="modern-list">
+              <div class="grid-category-thumb">
+                <a href="#" class="overlay-cate"><img src="<?= base_url('uploads/rumah/') . $p['banner']; ?>" class="img-responsive" alt=""></a>
+                <div class="property_meta simple">
+                  <?php if ($p['status_property'] == 1) { ?>
+                    <a href="#" class="cate-trix theme-cl">Dijual</a>
+                  <?php } else { ?>
+                    <a href="#" class="cate-trix theme-cl">Sewa</a>
+                  <?php } ?>
+                </div>
+              </div>
+              <div class="modern-list-content">
+                <div class="listing-content-wrap smalls text-center">
+                  <h4 class="lst-title"><a href="#"><?= $p['nama_pemilik_rumah'] ?></a>
+                  </h4>
+                  <p><?= $p['alamat_lengkap'] ?></p>
+                  <p class="btn btn-sm rounded-custom py-2 px-2 mt-2"><?= $p['harga'] ?></p>
+                </div>
+                <div class="listing-footer-wrap property-lists mt-2 d-flex justify-content-center">
+                  <div class="property-lists d-flex justify-content-center">
+                    <ul>
+                      <li><span class="flatcons"><img src="<?= base_url() ?>global/img/color-bed.svg" alt=""></span><?= $p['jumlah_kamar'] ?> Bed</li>
+                      <li><span class="flatcons"><img src="<?= base_url() ?>global/img/color-bathroom.svg" alt=""></span><?= $p['kamar_mandi'] ?> Bath</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      <?php } ?>
+      <!-- Single Property -->
 
-				<div class="row">
-					<?php foreach($populer as $p){?>
-						<div class="col-lg-4 col-md-6 col-sm-12">
-						<div class="list-slide-box">
-							<div class="modern-list">
-								<div class="grid-category-thumb">
-									<a href="#" class="overlay-cate"><img src="<?= base_url('uploads/rumah/') . $p['banner']; ?>"
-											class="img-responsive" alt=""></a>
-									<div class="property_meta simple">
-									<?php if($p['status_property'] == 1){?>
-										<a href="#" class="cate-trix theme-cl">Dijual</a>
-									<?php }else { ?>
-										<a href="#" class="cate-trix theme-cl">Sewa</a>
-									<?php } ?>
-									</div>
-								</div>
-								<div class="modern-list-content">
-									<div class="listing-content-wrap smalls text-center">
-										<h4 class="lst-title"><a href="#"><?=$p['nama_pemilik_rumah']?></a>
-										</h4>
-										<p><?= $p['alamat_lengkap']?></p>
-										<p class="btn btn-sm rounded-custom py-2 px-2 mt-2"><?= $p['harga']?></p>
-									</div>
-									<div class="listing-footer-wrap property-lists mt-2 d-flex justify-content-center">
-										<div class="property-lists d-flex justify-content-center">
-											<ul>
-												<li><span class="flatcons"><img src="<?= base_url()?>global/img/color-bed.svg"
-															alt=""></span><?= $p['jumlah_kamar']?> Bed</li>
-												<li><span class="flatcons"><img src="<?= base_url()?>global/img/color-bathroom.svg"
-															alt=""></span><?= $p['kamar_mandi']?> Bath</li>
-											</ul>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<?php } ?>
-					<!-- Single Property -->
-				
-				</div>
+    </div>
 
-			</div>
+  </div>
 
-			<div class="row">
-				<div class="col-lg-12 col-md-12 col-sm-12 text-center mt-3">
-					<a href="#" class="btn btn-theme bg-1">Selengkapnya</a>
-				</div>
-			</div>
+  <div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 text-center mt-3">
+      <a href="#" class="btn btn-theme bg-1">Selengkapnya</a>
+    </div>
+  </div>
 
-	</div>
-	</section>
-	<div class="clearfix"></div>
+  </div>
+</section>
+<div class="clearfix"></div>
 
 
-	<a id="back2Top" class="top-scroll" title="Back to top" href="#"><i class="fas fa-angle-up"></i></a>
+<a id="back2Top" class="top-scroll" title="Back to top" href="#"><i class="fas fa-angle-up"></i></a>
 
 
-	</div>
+</div>
